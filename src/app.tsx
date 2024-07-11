a361f18e3a89368b02b4b7c762651973005a9b7b
LucasAmaralll <lukinhafaustino2001@gmail.com>
2024-07-11 18:13:04 -0300

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from "lucide-react"
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus, User } from "lucide-react"
 import { FormEvent, useState } from "react"
 
 export function App() {
   const [isGuestInputOpen, setIsGuestInputOpen] = useState (false);
   const [isGuestModalOpen, setIsGuestModalOpen] = useState(false);
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false) 
+
   const [emailsToInvite, setEmailsToInvite] = useState([
     'lukinhafaustino2001@gmail.com'
   ]);
@@ -44,6 +46,14 @@ export function App() {
     ])
 
     event.currentTarget.reset()
+  }
+
+  function OpenConfirmTripModal () {
+    setIsGuestModalOpen(true)
+  }
+
+  function closeConfirmTripModal () {
+    setIsConfirmTripModalOpen (false)
   }
 
   function removeEmailFromInvites (emailToRemove:string) {
@@ -161,7 +171,49 @@ export function App() {
             </div>
           </div>
         )}
-    </div>
+
+
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+                    <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+                      <div className="space-y-2">
+                        <div className="flex items-center justify-between">
+                          <h2 className="text-lg font-semibold">Confirmar criação de viagem</h2>
+                          <button type="button" onClick={closeGuestModal}>
+                            <X className="size-5 text-zinc-400"/>
+                          </button>
+                        </div>
+                          <p className="text-sm text-zinc-400">
+                            Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">Florianópolis, Brasil</span> nas datas de <span className="font-semibold text-zinc-100"> 16 a 27 de Agosto de 2024</span> prrencha seus dados abaixo:
+                            </p>
+                        </div>
+
+                        <form onSubmit={addNewEmailToInvite} className="space-y-3">
+                          <div className="h-14 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
+                            <User className="text-zinc-400 size-5" />
+                            <input  
+                            type="text" 
+                            name ="name" 
+                            placeholder="Seu nome completo" 
+                            className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"/>
+                          </div>
+
+                          <div className="h-14 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
+                            <User className="text-zinc-400 size-5" />
+                            <input  
+                            type="email" 
+                            name ="email" 
+                            placeholder="Seu e-mail pessoal" 
+                            className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"/>
+                          </div>
+
+                          <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
+                              Convidar
+                              <Plus className="size-5" />
+                            </button>
+                        </form>   
+                    </div>
+                  </div>
+            </div>
   )
 }
 
