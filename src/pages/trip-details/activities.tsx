81de42cd738d994f48e2fc9ae6f5399b7127e13d
LucasAmaralll <lukinhafaustino2001@gmail.com>
2024-07-13 14:20:18 -0300

    refresh details

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,6 +1,27 @@
 import { CircleCheck } from "lucide-react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+
+interface Activity {
+    date: string
+    activities: {
+        id: string
+        title: string
+        occurs_at: string
+    }[]
+
+}
 
 export function Activities () {
+
+    const { tripId } = useParams();
+    const [activities, setActivities] = useState<Activity[]>([]);
+
+    useEffect(() => {
+        api.get(`/trips/${tripId}/activities`).then(response => setActivities(response.data.activities));
+    }, [tripId]);
+
     return (
         <div className="space-y-8">
                         <div className="space-y-2.5">
